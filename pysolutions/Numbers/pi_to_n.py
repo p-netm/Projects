6409d2d8cbb80aae8f30d5687f95fd945bbd7ff8
= <pmuriuki98@gmail.com>
2018-06-23 20:59:56 +0300

    solved exponential to n precision

--- pysolutions/Numbers/pi_to_n.py
+++ pysolutions/Numbers/pi_to_n.py
@@ -3,10 +3,16 @@ Find PI to the Nth Digit - Enter a number and have the program generate PI up to
 Keep a limit to how far the program will go."""
 import math
 
+def put_precision(constant):
+    precision = (input("Input your chosen precision level: "))
+    limit = 20
+    if not precision.isdigit():
+        raise ValueError("Unrecognized data format")
+    elif int(precision) > limit:
+        print("Precision bypassed the limit")
+    else:
+        precision = int(precision)
+        print(round(constant, precision))
 
-precision = (input("Input your chosen precision level: "))
-if not precision.isdigit():
-    raise ValueError("Unrecognized data format")
-else:
-    precision = int(precision)
-    print(round(math.pi, precision))
+if __name__ == "__main__":
+    put_precision(math.pi)
